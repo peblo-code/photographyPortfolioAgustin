ea6ef7eaaa798d7eef0401b9d864db6fc4350321
Pablo <pabling.benedix@gmail.com>
2020-09-03 18:35:42 -0400

    carga de imagenes a traves de json

--- javascript/index.js
+++ javascript/index.js
@@ -20,3 +20,51 @@ btnNext.onclick = function() {
 btnHome.onclick = function() {
     selector.home();
 }
+
+// carga y renderizado de imagenes
+
+const initURL = '../images'
+const URL = '../images/images.json'
+let data;
+let response;
+
+function setElementAttribute(element, attributes) {
+    for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function renderImage(maxImg) {
+    maxImg.forEach(img => {
+        let $section = document.getElementById('post-list')
+        let $figure = document.createElement('figure');
+        let $image = document.createElement('img');
+        $section.appendChild($figure);
+        $figure.appendChild($image);
+        
+        const attrImage = {
+            'src': `${initURL}${data.imgSobreMi[img]}`
+        }
+
+         const attrFigure = {
+            'class': 'post-image'
+        }
+        setElementAttribute($figure, attrFigure)
+        setElementAttribute($image, attrImage)
+        //document.body.appendChild($figure);
+    });
+}
+
+async function getImage(URL) {
+    try {
+        response = await fetch(URL); //respuesta
+        data = await response.json(); //datos
+        let maxImg = Object.keys(data.imgSobreMi) //sacando array de datos
+        renderImage(maxImg) // renderizamos las imagenes
+        
+    } catch(error) {
+        console.error(`Sucedio un error: ${error}`);
+    }
+}
+
+getImage(URL);
